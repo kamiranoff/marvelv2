b38ce6503158e32c46fc4f0138118922fc354e8b
Kevin Amiranoff <kamiranoff@gmail.com>
2016-06-13 19:17:06 +0200

    remove js and .map from client from  git

--- client/dev/index.js
+++
@@ -1,8 +0,0 @@
-/* Avoid: 'error TS2304: Cannot find name <type>' during compilation */
-///<reference path="../../typings/globals/es6-shim/index.d.ts"/>
-"use strict";
-var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
-var http_1 = require('@angular/http');
-var app_component_1 = require('./marvel-app/app.component');
-var router_deprecated_1 = require("@angular/router-deprecated");
-platform_browser_dynamic_1.bootstrap(app_component_1.AppComponent, [http_1.HTTP_PROVIDERS, router_deprecated_1.ROUTER_PROVIDERS]);
