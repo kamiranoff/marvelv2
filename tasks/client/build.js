558b29c9dbfd4f700ff7432ea2e0f6461f11aa3a
Kevin Amiranoff <kamiranoff@gmail.com>
2016-06-13 19:34:42 +0200

    fixes prod build

--- tasks/client/build.js
+++ tasks/client/build.js
@@ -9,10 +9,10 @@ gulp.task(tasks.CLIENT_BUILD_DEV, [
 
 gulp.task(tasks.CLIENT_BUILD_DIST, () => {
   runSequence(tasks.CLIENT_BUILD_TS,
-              //tasks.CLIENT_UNIT_TEST,
-              tasks.CLIENT_DEL_DIST,
-              tasks.CLIENT_COPY,
-              tasks.CLIENT_VIEWS_DIST,
-              [tasks.CLIENT_IMAGE_DIST,
-               tasks.CLIENT_JS_DIST]);
+    //tasks.CLIENT_UNIT_TEST,
+    tasks.CLIENT_DEL_DIST,
+    tasks.CLIENT_COPY,
+    tasks.CLIENT_VIEWS_DIST,
+    [tasks.CLIENT_IMAGE_DIST,
+      tasks.CLIENT_JS_DIST]);
 });
--- tasks/client/build_html.js
+++ tasks/client/build_html.js
@@ -7,7 +7,10 @@ const VIEWS = path.DIST + '**/*.html';
 
 gulp.task(tasks.CLIENT_VIEWS_DIST, () => {
   return gulp.src(VIEWS, {base: path.DIST})
-             .pipe(rev())
-			       .pipe(htmlmin({collapseWhitespace: false}))
-             .pipe(gulp.dest(path.DIST));
+    .pipe(rev())
+    .pipe(htmlmin({
+      collapseWhitespace: false,
+      caseSensitive: true
+    }))
+    .pipe(gulp.dest(path.DIST));
 });
